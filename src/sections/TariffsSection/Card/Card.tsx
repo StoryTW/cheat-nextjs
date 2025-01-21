a09962c6591b6fd12e546d71a2bbf333c52a0fea
parienko_sergej <parienko_sergej@enot.work>
2025-01-21 19:26:25 +0300

    fix

--- src/sections/TariffsSection/Card/Card.tsx
+++ src/sections/TariffsSection/Card/Card.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import IconCard from '@/assets/icons/pay_methods/card.svg?react';
@@ -8,6 +8,9 @@ import IconYoomoney from '@/assets/icons/pay_methods/yoomoney.svg?react';
 import IconEth from '@/assets/icons/pay_methods/eth.svg?react';
 import IconTon from '@/assets/icons/pay_methods/ton.svg?react';
 import { Slider } from '@/components/Slider/Slider';
+import IconArrow from '@/assets/icons/icon-arrow-s.svg?react';
+import IconCheckbox from '@/assets/icons/checkbox.svg?react';
+import { AnimatePresence } from 'motion/react';
 
 interface ICard {
   data: any;
@@ -32,7 +35,43 @@ const PAY_ARRAY = [
   },
 ];
 
+const options = [
+  {
+    id: '1',
+    name: 'Опция 1',
+  },
+  {
+    id: '2',
+    name: 'Опция 2',
+  },
+  {
+    id: '3',
+    name: 'Опция 3',
+  },
+  {
+    id: '4',
+    name: 'Опция 4',
+  },
+  {
+    id: '5',
+    name: 'Опция 5',
+  },
+];
+
 export const Card = ({ data, active }: ICard) => {
+  const [open, setOpen] = useState(false);
+  const toggle = () => setOpen((prev) => !prev);
+
+  const [selectedOptions, setSelectedOptions] = useState<{ id: string; name: string }[]>([]);
+
+  const handleSelect = (item: { id: string; name: string }) => {
+    setSelectedOptions((prev) =>
+      prev.some((el) => el.id === item.id)
+        ? prev.filter((el) => el.id !== item.id)
+        : [...prev, item],
+    );
+  };
+
   return (
     <div
       className={clsx(styles.root, {
@@ -50,8 +89,37 @@ export const Card = ({ data, active }: ICard) => {
           </h3>
           <div className={styles.content}>
             <div className={styles.info}>Информация</div>
-            <div>asd</div>
+            <div className={styles.dropdown} onClick={toggle}>
+              <div className={styles.dropdownText}>Дополнительно</div>
+              <div
+                className={clsx(styles.arrow, {
+                  [styles.open]: open,
+                })}
+              >
+                <IconArrow />
+              </div>
+            </div>
           </div>
+          <AnimatePresence>
+            {open && (
+              <div className={styles.ddList}>
+                {options.map((item, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className={clsx(styles.option, {
+                        [styles.selected]: selectedOptions.some((el) => el.id === item.id),
+                      })}
+                      onClick={() => handleSelect(item)}
+                    >
+                      {item.name}
+                      {selectedOptions.some((el) => el.id === item.id) && <IconCheckbox />}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </AnimatePresence>
         </div>
 
         <div className={styles.foot}>
